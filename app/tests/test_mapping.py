eedfd64efdcc680480c59614fc7a362e64ddf6ae
Fernando-Ares-Robledo <119211161+Fernando-Ares-Robledo@users.noreply.github.com>
2026-02-14 10:41:51 +0100

    Delete app directory

--- app/tests/test_mapping.py
+++
@@ -1,7 +0,0 @@
-from app.core.mapping import paper_word_index
-
-
-def test_paper_word_index_mapping():
-    assert paper_word_index(0, 0) == 0
-    assert paper_word_index(1, 0) == 256
-    assert paper_word_index(15, 255) == 4095
